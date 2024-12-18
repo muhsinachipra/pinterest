822e0f187925283d508497a0d9e03ac3e6524129
Muhsin Achipra <muhsinachipra@gmail.com>
2024-12-18 20:57:01 +0530

    Update vite.config.js: comment out proxy settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,4 @@
+// frontend\vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -5,10 +6,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-      },
-    },
+    // proxy: {
+    //   "/api": {
+    //     target: "http://localhost:3000",
+    //   },
+    // },
   },
 });
